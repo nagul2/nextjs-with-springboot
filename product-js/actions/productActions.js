4a129063afbb8e5b306e65af6db0665928a9a402
nagul2 <hayani0000@gmail.com>
2026-02-14 15:12:24 +0900

    상품 예제 종료

--- product-js/actions/productActions.js
+++ product-js/actions/productActions.js
@@ -39,3 +39,65 @@ export const postProduct = async (prevState, formData) => {
 
   return { message: "Product created successfully", result: "success" };
 };
+
+export const putProduct = async (prevState, formData) => {
+  console.log("putProduct called");
+
+  const pno = formData.get("pno");
+
+  const updatedFormData = new FormData();
+  updatedFormData.append("pname", formData.get("pname"));
+  updatedFormData.append("price", formData.get("price"));
+  updatedFormData.append("sale", formData.get("sale"));
+
+  const fileNames = formData.getAll("fileNames");
+  console.log("fileNames length: " + fileNames);
+  if (fileNames.length > 0) {
+    fileNames.forEach((fileName) => {
+      updatedFormData.append("fileNames", fileName);
+    });
+  }
+  // Append all files to the new FormData object
+  const files = formData.getAll("files");
+  if (files.length > 0) {
+    files.forEach((file) => {
+      console.log("----------------------------------", file);
+      if (file && file.size > 0) {
+        updatedFormData.append("files", file);
+      }
+    });
+  }
+  // Make the fetch request
+  const response = await fetch(`http://localhost:8080/api/products/${pno}`, {
+    method: "PUT",
+    body: updatedFormData,
+  });
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    console.error("Failed to update product:", errorText);
+    return { message: "Failed to update product", result: "fail" };
+  }
+
+  revalidatePath(`/product/view/${pno}`);
+
+  return { message: "Product updated successfully", result: "success" };
+};
+
+export const deleteProduct = async (prevState, formData) => {
+  const pno = formData.get("pno");
+
+  console.log("deleteProduct called with pno:", pno);
+
+  const response = await fetch(`http://localhost:8080/api/products/${pno}`, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to delete product");
+  }
+
+  revalidatePath(`/product/view/${pno}`);
+
+  return { message: "Product deleted successfully", result: "success" };
+};
